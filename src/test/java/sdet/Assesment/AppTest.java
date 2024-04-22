8c9273dffdc1a6927b55997553c1c43b0c0a1414
Prasad G <guntakavprasad@github.com>
2024-04-22 16:58:34 +0530

    first commit

---
+++ src/test/java/sdet/Assesment/AppTest.java
@@ -0,0 +1,18 @@
+package sdet.Assesment;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+  @Test	
+	public void loginTest() {
+		
+		System.out.println("test NG RUN");
+	}
+
+	
+	
+}
